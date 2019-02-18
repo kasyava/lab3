e96c37079e6a8666baa431dccc2461e6282a72e6
kasyava <budo_slava@mail.ru>
2019-02-18 14:28:31 +0600

    добавил файл config.js для соединения с базой, написал первоначальный макет файла server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+
+
+const users = require("./app/users");
+
+
+const config =  require("./config");
+
+const port = 8000;
+const app = express();
+
+mongoose.set('useCreateIndex', true);
+mongoose.connect(config.db.url + '/' + config.db.name, {useNewUrlParser: true });
+const db = mongoose.connection;
+
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+db.once('open', () => {
+
+
+    app.use('/users', users);
+
+    app.listen(port, () => console.log(`Server started on ${port}`));
+});
